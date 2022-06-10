823647a2a98a462377275b0c7c1324951ffa37db
Sangchun Ha (Patrick) <seomk9896@naver.com>
2022-06-11 02:08:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='squeezeformer',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='An Efficient Transformer for Automatic Speech Recognition',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Ha Sangchun',
     author_email='seomk9896@gmail.com',
     url='https://github.com/upskyy/Squeezeformer',
-    keywords=['asr', 'speech_recognition', 'artificial intelligence', 'transformer'],
+    keywords=['asr', 'speech_recognition', 'artificial intelligence'],
     python_requires='>=3.6',
     install_requires=[
         'torch>=1.4.0',
